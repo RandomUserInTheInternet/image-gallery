083b4c2d5f97f8e6a0d7b3da688354c5790ed6e3
RandomUserInTheInternet <135203715+RandomUserInTheInternet@users.noreply.github.com>
2023-06-01 18:18:13 +0800

    Update admin.js

--- image-gallery/admin.js
+++ image-gallery/admin.js
@@ -1,3 +1,6 @@
-// Initialize Netlify Identity
-var netlifyIdentity = window.netlifyIdentity;
-netlifyIdentity.init();
+document.getElementById('logoutButton').addEventListener('click', logout);
+
+function logout() {
+  // Perform logout logic here (e.g., clearing session, redirecting to login page)
+  window.location.href = 'index.html'; // Redirect to the login page
+}
